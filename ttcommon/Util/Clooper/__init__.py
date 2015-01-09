95ef9329bc8993cb20da5441fad2db9da15f9f70
Gonzalo Odiard <godiard@gmail.com>
2015-01-09 14:18:18 -0300

    Use new syntax for exceptions in Clooper/__init__.py

--- ttcommon/Util/Clooper/__init__.py
+++ ttcommon/Util/Clooper/__init__.py
@@ -14,8 +14,8 @@ for i in os.listdir(_root_path):
             logging.debug('use %s blobs' % path)
             _sys_path = None
             break
-        except Exception, e:
+        except Exception as e:
             logging.debug('skip %s blobs: %s' % (path, e))
 
 if _sys_path:
-    raise('cannot find proper binary blobs')
+    raise BaseException('cannot find proper binary blobs')
