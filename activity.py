d750d29e4a859a2847570927458ab4e90e03eda2
Gonzalo Odiard <godiard@gmail.com>
2015-01-21 16:01:19 -0500

    When start to record, reset the cursor in the notes view

--- activity.py
+++ activity.py
@@ -924,6 +924,7 @@ class SimplePianoActivity(activity.Activity):
             self.recorded_keys = []
             self.recording = True
             self.record_button.props.icon_name = 'media-playback-stop'
+            self._notes_view.reset_counter()
         else:
             self.recording = False
             self.record_button.props.icon_name = 'media-record'
