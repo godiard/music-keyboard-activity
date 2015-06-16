ca18479ed4ba285edb669300b3d53a0263bdb313
Gonzalo Odiard <godiard@gmail.com>
2015-06-16 10:30:26 -0300

    Change button_se test to check matching parameters

--- test/button_se.py
+++ test/button_se.py
@@ -8,10 +8,10 @@ ksmps = 10
 nchnls = 1
 
 ; Instrument #1.
-instr 1
+instr 1000
    kamp = 30000
    kcps = 1
-   ifn = 1
+   ifn = 10
    ibas = 1
 
    ; Play the audio sample stored in Table #1.
@@ -23,16 +23,20 @@ c = csnd6.Csound()
 c.SetOption("-odac")
 c.CompileOrc(orc)
 c.Start()
-c.InputMessage('f 1 0 131072 1 "beats.wav" 0 4 0')
+
+# the first parameter should be the same as the ifn value in the instrument
+c.InputMessage('f 10 0 131072 1 "beats.wav" 0 4 0')
 
 perfThread = csnd6.CsoundPerformanceThread(c)
 perfThread.Play()
 
 def clicked_cb(button, c):
-    python_array = [1.0, 0.0, 2.0]
+    python_array = [1000,  # this need be the instrument number
+                    0.0, 2.0]
     csnd_array = csnd6.doubleArray(len(python_array))
     for n in range(len(python_array)):
         csnd_array[n] = python_array[n]
+    print python_array
     c.ScoreEvent('i', csnd_array, len(python_array))
     # TODO: these are to try avoid seg fault at activity close (not enough)
     # del csnd_array
